ab87e92f58ed0dc32fa4c2d45766cdc1796f40e5
Dennis Chiuten <dennis@work.flowers>
2025-09-18 11:23:28 +0800

    Fixes for WFOF-303

--- daily_cac.sql
+++ daily_cac.sql
@@ -1,30 +1,63 @@
 WITH marketing AS (
 	SELECT
 		date,
-		UPPER(country_code) AS country,
+		country_code AS country,
+		COALESCE(condition, 'N/A') AS condition,
 		SUM(cost_usd) AS marketing_spend
 	FROM cac.marketing_spend
-	GROUP BY 1,2
+	GROUP BY 1,2,3
+),
+
+first_day AS (
+	SELECT 
+		customer_id, 
+		MIN(obs_date) AS first_obs_date
+  FROM all_stripe.subscription_metrics
+  WHERE 
+  	mrr_usd > 0
+  GROUP BY 1
 ),
 
 acq_dates AS (
 	SELECT
-		sh.region,
 		sh.customer_id,
-		MIN(sh.obs_date) AS acquired_date
+		sh.region,
+		COALESCE(sh.condition, 'N/A') AS condition,
+		sh.obs_date AS acquired_date,
+		sh.mrr_usd,
+		ROW_NUMBER() OVER (
+			PARTITION BY sh.customer_id
+			ORDER BY sh.mrr_usd DESC
+		) AS rn
 	FROM all_stripe.subscription_metrics AS sh
+	JOIN first_day AS fd
+		ON sh.customer_id = fd.customer_id
+		AND sh.obs_date = fd.first_obs_date
 	WHERE 
 		sh.mrr_usd > 0
-	GROUP BY 1,2
+),
+
+acquisitions AS (
+	SELECT 
+		region,
+		condition,
+		acquired_date,
+		COUNT(DISTINCT customer_id) AS n_new_customers
+	FROM acq_dates
+	WHERE 
+		rn = 1
+	GROUP BY 1,2,3
+	
 )
 
 SELECT
 	mar.date,
 	mar.country,
 	mar.marketing_spend,
-	COUNT(DISTINCT ad.customer_id) AS n_new_customers
+	mar.condition,
+	COALESCE(a.n_new_customers,0)
 FROM marketing AS mar
-LEFT JOIN acq_dates AS ad
-	ON DATE(mar.date) = DATE(ad.acquired_date)
-	AND LOWER(mar.country) = LOWER(ad.region)
-GROUP BY 1,2,3
+LEFT JOIN acquisitions AS a
+	ON mar.date = a.acquired_date
+	AND LOWER(mar.country) = LOWER(a.region)
+	AND mar.condition = a.condition
--- daily_net_new_cac.sql
+++ daily_net_new_cac.sql
@@ -11,34 +11,57 @@ WITH marketing AS (
 	GROUP BY 1,2,3
 ),
 
+first_day AS (
+	SELECT 
+		customer_id, 
+		MIN(obs_date) AS first_obs_date
+  FROM all_stripe.subscription_metrics
+  WHERE 
+  	mrr_usd > 0
+  	AND new_existing = 'New'
+  GROUP BY 1
+),
+
 acq_dates AS (
 	SELECT
-		sh.region,
 		sh.customer_id,
+		sh.region,
 		COALESCE(sh.condition, 'N/A') AS condition,
-		sh.obs_date AS acquired_date
+		sh.obs_date AS acquired_date,
+		sh.mrr_usd,
+		ROW_NUMBER() OVER (
+			PARTITION BY sh.customer_id
+			ORDER BY sh.mrr_usd DESC
+		) AS rn
 	FROM all_stripe.subscription_metrics AS sh
+	JOIN first_day AS fd
+		ON sh.customer_id = fd.customer_id
+		AND sh.obs_date = fd.first_obs_date
 	WHERE 
 		sh.mrr_usd > 0
-		AND sh.new_existing = 'New'
-	QUALIFY ROW_NUMBER() OVER(
-		PARTITION BY 
-			sh.customer_id 
-		ORDER BY 
-			sh.obs_date DESC,
-			sh.mrr_usd DESC
-		) = 1
+),
+
+acquisitions AS (
+	SELECT 
+		region,
+		condition,
+		acquired_date,
+		COUNT(customer_id) AS n_new_customers
+	FROM acq_dates
+	WHERE 
+		rn = 1
+	GROUP BY 1,2,3
 )
+
 
 SELECT
 	mar.date,
 	mar.country,
 	mar.marketing_spend,
 	mar.condition,
-	COUNT(DISTINCT ad.customer_id) AS n_new_customers
+	COALESCE(a.n_new_customers,0) AS n_new_customers
 FROM marketing AS mar
-LEFT JOIN acq_dates AS ad
-	ON DATE(mar.date) = DATE(ad.acquired_date)
-	AND LOWER(mar.country) = LOWER(ad.region)
-	AND mar.condition = ad.condition
-GROUP BY 1,2,3,4
+LEFT JOIN acquisitions AS a
+	ON mar.date = a.acquired_date
+	AND LOWER(mar.country) = LOWER(a.region)
+	AND mar.condition = a.condition	
