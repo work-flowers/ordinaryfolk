f8d2615d3810c5dd8b8ffad5c56b36835be544e7
Dennis Chiuten <dennis@work.flowers>
2025-10-27 15:54:32 +0800

    Part of WFOF-326. Enhance monthly revenue query with customer revenue and first purchase logic
    Refactored the SQL to introduce customer_revenue and first_purchase CTEs, enabling more granular aggregation and tracking of customer conditions and revenue. The final output now joins on first purchase data and uses a new 'final' CTE for improved clarity and extensibility.

--- monthly_revenue_by_customer.sql
+++ monthly_revenue_by_customer.sql
@@ -9,28 +9,63 @@ WITH sub_starts AS (
 		ON h.id = i.subscription_id
 	LEFT JOIN all_stripe.plan AS pl
 		ON i.plan_id = pl.id
+),
+
+customer_revenue AS (
+	SELECT
+		cm.customer_id,
+		cm.condition,
+		cm.purchase_date,
+		SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd)) AS revenue		
+	FROM finance_metrics.contribution_margin AS cm
+	WHERE
+		1 = 1
+		AND cm.customer_id IS NOT NULL
+		AND cm.condition IS NOT NULL
+	GROUP BY 1,2,3
+),
+
+first_purchase AS (
+	SELECT 
+		cr.*,
+		ROW_NUMBER() OVER (
+			PARTITION BY 
+				cr.customer_id
+			ORDER BY
+				cr.purchase_date DESC,
+				cr.revenue DESC
+			) AS rn
+	FROM customer_revenue AS cr
+),
+
+final AS (
+	SELECT
+		DATE_TRUNC(cm.purchase_date, MONTH) AS purchase_month,
+		cm.sales_channel,
+		cm.region,
+		cm.new_existing,
+		cm.customer_id,
+		cm.charge_id,
+		cm.subscription_id,
+		fp.condition,
+		cm.product_name,
+		cm.billing_reason,
+		cm.purchase_type,
+		sub_starts.create_date AS subscription_created,
+		sub_starts.interval,
+		sub_starts.interval_count,
+		SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd)) AS revenue_usd,
+		MIN(MIN(DATE_TRUNC(cm.purchase_date, MONTH))) OVER(PARTITION BY cm.customer_id) AS cohort_month
+	FROM finance_metrics.contribution_margin AS cm
+	LEFT JOIN first_purchase AS fp
+		ON cm.customer_id = fp.customer_id
+		AND fp.rn = 1
+	LEFT JOIN sub_starts
+		ON cm.subscription_id = sub_starts.subscription_id
+	WHERE
+		1 = 1
+	GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14
 )
 
-SELECT
-	DATE_TRUNC(cm.purchase_date, MONTH) AS purchase_month,
-	cm.sales_channel,
-	cm.region,
-	cm.new_existing,
-	cm.customer_id,
-	cm.charge_id,
-	cm.subscription_id,
-	cm.condition,
-	cm.product_name,
-	cm.billing_reason,
-	cm.purchase_type,
-	sub_starts.create_date AS subscription_created,
-	sub_starts.interval,
-	sub_starts.interval_count,
-	SUM(COALESCE(cm.line_item_amount_usd, cm.total_charge_amount_usd)) AS revenue_usd,
-	MIN(MIN(DATE_TRUNC(cm.purchase_date, MONTH))) OVER(PARTITION BY customer_id) AS cohort_month
-FROM finance_metrics.contribution_margin AS cm
-LEFT JOIN sub_starts
-	ON cm.subscription_id = sub_starts.subscription_id
-WHERE
-	1 = 1
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14
+SELECT *
+FROM final
